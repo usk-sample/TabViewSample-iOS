2fd8eac2b7667041221050b6a39284d5b9c3508d
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-10-16 13:57:38 +0900

    Initial Commit

---
+++ TabViewSample/TabViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewSampleApp.swift
+//  TabViewSample
+//
+//  Created by Yusuke Hasegawa on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct TabViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
